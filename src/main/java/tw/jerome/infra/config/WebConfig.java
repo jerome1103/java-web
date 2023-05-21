e76a949b93f1cd13df63a47cb5701ae143752022
Jerome <jerome.lin.1103@gmail.com>
2023-05-21 22:27:35 +0800

    refactor:修改了URL"/"直接返回swagger-UI

--- src/main/java/tw/jerome/infra/config/WebConfig.java
+++ src/main/java/tw/jerome/infra/config/WebConfig.java
@@ -16,23 +16,23 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		// 註冊首頁
-		registry.addViewController("/").setViewName("login");
+//		registry.addViewController("/").setViewName("login");
 		registry.addViewController("/index.html").setViewName("login");
 		registry.addViewController("/main.html").setViewName("dashboard");
 	}
 
 	// 註冊攔截器
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-    	registry.addInterceptor(new LoginHandlerInterceptor())
-    			//要攔截的路徑
-    			.addPathPatterns("/**")
-    			//這個攔截器例外的路徑
-    			.excludePathPatterns(
-    					"/index.html","/","/user/login",
-    					"/**/*.css","/**/*.js",
-    					"/**/*.svg","/**/*.png","/**/*.jpg");
-	}
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//    	registry.addInterceptor(new LoginHandlerInterceptor())
+//    			//要攔截的路徑
+//    			.addPathPatterns("/**")
+//    			//這個攔截器例外的路徑
+//    			.excludePathPatterns(
+//    					"/index.html","/","/user/login",
+//    					"/**/*.css","/**/*.js",
+//    					"/**/*.svg","/**/*.png","/**/*.jpg");
+//	}
 
 	// 語系設定
 	@Bean
